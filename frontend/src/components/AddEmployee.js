3fbbaf4fc845f7c2c2a36565981e88257d6112ec
Saranraj S <900570@hepl.local>
2025-10-17 09:50:07 +0530

    Addemp

--- frontend/src/components/AddEmployee.js
+++ frontend/src/components/AddEmployee.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 export default function AddEmployee() {
