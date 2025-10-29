a99cd775c8c28c95e190bfadaed732d6fd1ab34e
Saranraj S <900570@hepl.local>
2025-10-29 13:36:09 +0530

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import AddEmployee from "./components/AddEmployee";
+import BulkUpload from "./BulkUpload/BulkUpload";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/add-employee" element={<AddEmployee />} />
+        <Route path="/bulk-upload" element={<BulkUpload />} />
       </Routes>
     </Router>
   );
